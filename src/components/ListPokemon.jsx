c1980fe4ba04674aa5fbabfef83e4ffbea2ac66e
Laceinstein <laceinstein@gmail.com>
2022-09-02 12:07:29 +0200

    correction de l'erreur d'affichage

--- src/components/ListPokemon.jsx
+++ src/components/ListPokemon.jsx
@@ -19,7 +19,9 @@ export default function ListPokemon(){
 
   console.log(pokemons)
   
-  const pokemonList = !pokemons.results ? <p>Erreur lors de la récupération des pokemons</p> : pokemons.results.map((pokemon, index) => <div key={index}><h2>{pokemon.name}</h2> <button onClick={() => history('/pokemen:id')}>Details</button> <br /></div>)
+  const pokemonList = !pokemons.results ? 
+  <p>Erreur lors de la récupération des pokemons</p> : 
+  pokemons.results.map((pokemon, index) => <div key={index}><h2>{pokemon.name}</h2> <button onClick={() => history('/pokemon:id')}>Details</button> <br /></div>)
   
   return(
     <div>
